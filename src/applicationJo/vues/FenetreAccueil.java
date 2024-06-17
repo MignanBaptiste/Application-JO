a96792f443eb7446d994b7a0a0829a66ccdb3f20
De oliveira Hugo <hugdeoliveira1@gmail.com>
2024-06-17 17:25:43 +0200

    Ajout de la fenetre d'accueil

---
+++ src/applicationJo/vues/FenetreAccueil.java
@@ -0,0 +1,26 @@
+package src;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Fenetre extends Application{
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox root = FXMLLoader.load(getClass().getResource("FenetreAcceuil.fxml"));
+        
+        Scene scene = new Scene(root); 
+        primaryStage.setTitle("Ma Fenêtre JavaFX"); // Titre de la fenêtre
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
