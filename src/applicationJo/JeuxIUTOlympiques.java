fcbfc209b03330be677ae1945bc83ef78217c405
valentin-hun <valentin.hun@etu.univ-orleans.fr>
2024-06-17 11:04:34 +0200

    début de l'application

---
+++ src/applicationJo/JeuxIUTOlympiques.java
@@ -0,0 +1,24 @@
+package applicationJo;
+
+import javafx.application.*;
+import javafx.stage.Stage;
+
+
+public class JeuxIUTOlympiques extends Application {
+
+    @Override
+    public void init() throws Exception {
+        // TODO Auto-generated method stub
+        super.init();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
