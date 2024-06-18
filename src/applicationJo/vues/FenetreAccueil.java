92f267ce262b1d7ca2106cc20eb484a99925ffbd
De oliveira Hugo <hugdeoliveira1@gmail.com>
2024-06-18 15:44:02 +0200

    Maj de fenetre Accueil, Inscription et Connexion

--- src/applicationJo/vues/FenetreAccueil.java
+++ src/applicationJo/vues/FenetreAccueil.java
@@ -3,6 +3,7 @@ package applicationJo.vues;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,7 +11,7 @@ public class FenetreAccueil extends Application{
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        VBox root = FXMLLoader.load(getClass().getResource("../../../fxml/FenetreAccueil.fxml"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("../../../fxml/FenetreAccueil.fxml"));
 
         Scene scene = new Scene(root); 
         primaryStage.setTitle("Ma Fenêtre JavaFX"); // Titre de la fenêtre
