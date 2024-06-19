50a4a493a17f5e7a2f9a61b8378274efd1ddfe93
MignanBaptiste <baptiste.mignan@etu.univ-orleans.fr>
2024-06-19 17:38:05 +0200

    modifs BDSelection

--- src/applicationJo/database/BDSelection.java
+++ src/applicationJo/database/BDSelection.java
@@ -10,20 +10,23 @@ import java.util.HashMap;
 import java.util.List;
 
 import jo.*;
+import jo.sport.*;
 
 public class BDSelection {
     private ConnexionMySQL connexion;
+    private JeuxOlympiques jo;
     private Statement st;
 
-    public BDSelection(ConnexionMySQL connexion){
+    public BDSelection(ConnexionMySQL connexion, JeuxOlympiques jo){
         this.connexion = connexion;
+        this.jo = jo;
     }
 
     public HashMap<List<String>, Integer> classementIndividuelle(Epreuve<Athlete> epreuve) throws SQLException{
         HashMap<List<String>, Integer> res = new HashMap<>();
         // Parcourir la liste des participants d'une épreuve
 
-        PreparedStatement ps = connexion.prepareStatement("select score, nomAthlete, prenomAthlete, nomPays from ATHLETE natural join EPREUVE where sexe = ? and nomSport = ? and categorieSport = ?");
+        PreparedStatement ps = connexion.prepareStatement("select score, nomAthlete, prenomAthlete, nomPays from ATHLETE natural join EPREUVE where sexe = ? and nomSport = ? and categorieSport = ? order by score");
         if (epreuve.getSexe().equals(Sexe.FEMME)){
             ps.setString(1, "F");
         }
@@ -45,7 +48,7 @@ public class BDSelection {
         HashMap<String, Integer> res = new HashMap<>();
         // Parcourir la liste des participants d'une épreuve
 
-        PreparedStatement ps = connexion.prepareStatement("select nomPays, score from EQUIPE natural join EPREUVE where sexe = ? and nomSport = ? and categorieSport = ?");
+        PreparedStatement ps = connexion.prepareStatement("select nomPays, score from EQUIPE natural join EPREUVE where sexe = ? and nomSport = ? and categorieSport = ? order by score");
         if (epreuve.getSexe().equals(Sexe.FEMME)){
             ps.setString(1, "F");
         }
@@ -59,5 +62,40 @@ public class BDSelection {
             res.put(rs.getString(1), rs.getInt(2));
         }
         return res;
+    }
+
+    // Méthode pour ajouter les médailles aux classements des pays de façon dynamique
+    public void ajoutMedailles() throws SQLException{
+        PreparedStatement ps = connexion.prepareStatement("select * from COLLECTIVE");
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+            Sexe sexe;
+            if (rs.getString(1).equals("F")){
+                sexe = Sexe.FEMME;
+            }
+            else{
+                sexe = Sexe.HOMME;
+            }
+            Sport sport;
+            if (rs.getString(2).equals("Athletisme")){
+                sport = new Athletisme(rs.getString(3));
+            }
+            else if (rs.getString(2).equals("Escrime")){
+                sport = new Escrime(rs.getString(3));
+            }
+            else if (rs.getString(2).equals("Handball")){
+                sport = new Handball(rs.getString(3));
+            }
+            else if (rs.getString(2).equals("Natation")){
+                sport = new Natation(rs.getString(3));
+            }
+            else if (rs.getString(2).equals("Volley-Ball")){
+                sport = new VolleyBall(rs.getString(3));
+            }
+            else{
+                sport = new Escrime(rs.getString(3));
+            }
+            Epreuve<Equipe> epreuve = new Epreuve<>(sexe, sport);
+        }
     }
 }
