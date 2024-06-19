da5247a0e7080049e98d09ea809e798804059d23
Corentin <corentin.thuault@etu.univ-orleans.fr>
2024-06-19 09:18:45 +0200

    modification du script bash

--- execappli.sh
+++ execappli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac --source-path ./src/applicationJo/vues/ -d bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls,javafx.fxml src/applicationJo/vues/JeuxIUTOlympiques.java
-java -cp bin/:img/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls,javafx.fxml applicationJo.vues.JeuxIUTOlympiques
+javac --source-path ./src/applicationJo/vues/ -d bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls,javafx.fxml src/applicationJo/vues/JeuxIUTOlympiques.java src/applicationJo/controleurs/AccueilControleur.java
+java -cp bin/:fxml/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls,javafx.fxml applicationJo.vues.JeuxIUTOlympiques
